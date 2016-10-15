c2eb82195b2d870e2e80bc79f800f000a154ce45
Ibrahim Uludag <ibrahimuludag@gmail.com>
2016-10-15 23:59:07 +0300

    SDI Application

---
+++ SDI/DocForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDI
+{
+    public partial class DocForm : Form
+    {
+        public DocForm()
+        {
+            InitializeComponent();
+        }
+
+        private static int _counter = 1;
+        public static Form CreateForm()
+        {
+            var form = new DocForm();
+            form.Text = "New Text Document " + (_counter++).ToString();
+            SdiApplication.Instance.ApplicationContext.MainForm = form;
+            form.Show();
+
+            return form;
+        }
+    }
+}
---
+++ SDI/SdiApplication.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace SDI
+{
+    public class SdiApplication : WindowsFormsApplicationBase
+    {
+        private static SdiApplication _instance = null;
+
+        public SdiApplication()
+        {
+            this.IsSingleInstance = true;
+            this.ShutdownStyle = ShutdownMode.AfterAllFormsClose;
+        }
+        public static SdiApplication Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new SdiApplication();
+                }
+                return _instance;
+            }
+        }
+
+        protected override void OnCreateMainForm()
+        {
+            DocForm.CreateForm();
+        }
+
+        protected override void OnStartupNextInstance(StartupNextInstanceEventArgs eventArgs)
+        {
+            DocForm.CreateForm();
+
+        }
+    }
+}
